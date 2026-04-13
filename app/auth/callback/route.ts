5c23548b3d77f6cdd91cdb332d2cd47ab16fe8a5
Johan Holmgren <elinstallatorholmgren@gmail.com>
2026-04-13 11:37:13 +0200

    bug fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 
+console.log("KEY:", process.env.STRIPE_SECRET_KEY);
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
